dbd371ebb6c723e7cf740332c1a61be124bffb76
Dave <dave.cadelina@outlook.com>
2024-02-25 00:39:43 +1100

    finished starting page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import ImageSlideshow from "@/components/images/image-slideshow";
 import styles from "./page.module.css";
 import Link from "next/link";
 
@@ -5,7 +6,9 @@ export default function Home() {
 	return (
 		<>
 			<header className={styles.header}>
-				<div className={styles.slideshow}></div>
+				<div className={styles.slideshow}>
+					<ImageSlideshow />
+				</div>
 				<div>
 					<div className={styles.hero}>
 						<h1>NextLevel Food for NextLevel Foodies</h1>
--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -8,24 +8,24 @@ import MainHeaderBackground from "./main-header-background";
 const MainHeader = () => {
 	return (
 		<>
-		<MainHeaderBackground />
-		<header className={styles.header}>
-			<Link href="/" className={styles.logo}>
-				<Image src={logoImg} alt="A plate with food on it" priority />
-				NextLevel Food
-			</Link>
+			<MainHeaderBackground />
+			<header className={styles.header}>
+				<Link href="/" className={styles.logo}>
+					<Image src={logoImg} alt="A plate with food on it" priority />
+					NextLevel Food
+				</Link>
 
-			<nav className={styles.nav}>
-				<ul>
-					<li>
-						<Link href="/meals">Browse Meals</Link>
-					</li>
-					<li>
-						<Link href="/contact">Foodies Community</Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
+				<nav className={styles.nav}>
+					<ul>
+						<li>
+							<Link href="/meals">Browse Meals</Link>
+						</li>
+						<li>
+							<Link href="/contact">Foodies Community</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
 		</>
 	);
 };
