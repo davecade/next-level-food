f5e9b00ce4bd226d8fbb30b8714043c36bbc8e99
Dave <dave.cadelina@outlook.com>
2024-02-25 16:44:27 +1100

    added meals grid

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,29 @@
 import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 
-const Meals = () => {
-	return <div>Meals</div>;
+const MealsPage = () => {
+	return (
+		<>
+			<header className={styles.header}>
+				<h1>
+					Delicious meals, create{" "}
+					<span className={styles.highlight}>by you</span>
+				</h1>
+				<p>
+					Choose your facvoriyte recipe and cook it yourself. It is east and
+					fun!
+				</p>
+				<p className={styles.cta}>
+					<Link href={"/meals/share"}>Share Your Favorite Recipe</Link>
+				</p>
+			</header>
+			<main className={styles.main}>
+				<MealsGrid meals={[]} />
+			</main>
+		</>
+	);
 };
 
-export default Meals;
+export default MealsPage;
