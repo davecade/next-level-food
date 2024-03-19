2ced05d1f685462adc80c84684910d3e57bc589a
Dave <dave.cadelina@outlook.com>
2024-03-19 22:08:11 +1100

    created server actions and also added slugify and xss

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,9 @@
+import { shareMeal } from "@/lib/action";
 import ImagePicker from "../image-picker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+
 	return (
 		<>
 			<header className={classes.header}>
@@ -11,7 +13,7 @@ export default function ShareMealPage() {
 				<p>Or any other meal you feel needs sharing!</p>
 			</header>
 			<main className={classes.main}>
-				<form className={classes.form}>
+				<form className={classes.form} action={shareMeal}>
 					<div className={classes.row}>
 						<p>
 							<label htmlFor="name">Your name</label>
